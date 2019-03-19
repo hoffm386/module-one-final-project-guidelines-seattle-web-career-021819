aab6242380e8197b4a716453f1e6e199b756b06a
hoffm386 <erinrhof@uw.edu>
2019-03-18 20:33:35 -0700

    Refactor to use ActiveRecord more
    Showing how to use things like `pluck` (instead of `map`), and grabbing a random sample of 5 houses or characters rather than a hard-coded set

--- app/CLI.rb
+++ app/CLI.rb
@@ -51,14 +51,13 @@ class CLI
 	    input = STDIN.gets.chomp
 		  if input.starts_with? "y" || input == "yes"
 		  	puts "Give me your best guess.."
-		    input = STDIN.gets.chomp
-			  if input == "2021"
+		    input = STDIN.gets.chomp.to_i
+				answer = Character.distinct.pluck(:name).count
+			  if input == answer
 			  	puts "Congratulations, you are officially a G.O.T NERD"
 			    is_running = false
 			  else
-			    puts "Nice try, the correct Answer is #{Character.all.map do |house_character|
-																										house_character.name
-																									end.uniq.count}"
+			    puts "Nice try, the correct Answer is #{answer}"
 					puts "Onward to the characters so you can NERD UP!!"
 					is_running = false
 				end
@@ -78,18 +77,26 @@ class CLI
 				===========================================================================
 				Enter a name for the Character to lookup or enter 0 to return to main menu.
 				===========================================================================
+				Suggested Searches (copy and paste works best!):
+				================================================
 			CHARACTER_NAME
+
+			Character.all.sample(5).pluck(:name).each do |character_name|
+				puts character_name
+				puts
+			end
+
+			puts "================================="
+
+
 			input = STDIN.gets.chomp
 			puts
 			puts
-			found_character = Character.find_by(name: input)
-			if found_character != nil
-				brethren = found_character.house.characters.map do |house_character|
-					if house_character.name != input
-						house_character.name
-					end
-				end.uniq
-				puts brethren
+			if Character.where(name: input).exists?
+				found_character = Character.find_by(name: input)
+				brethren = Character.where(house: found_character.house).where.not(name: input)
+				brethren_names = brethren.order(:name).distinct.pluck(:name)
+				puts brethren_names
 				puts "\n"
 				UpArrow.display
 				puts "#{found_character.name} has allegience with #{found_character.house.name}."
@@ -117,37 +124,49 @@ class CLI
 				Here is a list of Game of Thrones books.
 				========================================
 
-
-				Books:
 			LIST_OF_BOOKS
-			book_list = Book.all[0..2]
-			book_list << Book.all[4]
-			book_list << Book.all[7]
-			book_list.each_with_index do |book, i|
-				puts "#{i+1}. #{book.name}."
+
+			books_and_others = Book.all
+
+			main_book_list = [
+				books_and_others[0],
+				books_and_others[1],
+				books_and_others[2],
+				books_and_others[4],
+				books_and_others[7]
+			]
+
+			other_book_list = [
+				books_and_others[3],
+				books_and_others[5],
+				books_and_others[6],
+				books_and_others[8],
+				books_and_others[9],
+				books_and_others[10]
+			]
+
+			puts "Books:"
+			main_book_list.each_with_index do |book, index|
+				puts "#{index+1}. #{book.name}."
 			end
-			puts "\nGraphic Novels and Satellite stories: "
-			other_list = []
-			other_list << Book.all[3]
-			other_list << Book.all[5]
-			other_list << Book.all[6]
-			other_list << Book.all[8]
-			other_list << Book.all[9]
-			other_list << Book.all[10]
-			other_list.each_with_index do |book, i|
-				puts "#{i+6}. #{book.name}."
+
+			puts "\nGraphic Novels and Satellite stories:"
+			other_book_list.each_with_index do |book, index|
+				puts "#{index+6}. #{book.name}"
 			end
-			book_list << other_list
-			book_list = book_list.flatten
+
+			book_list = main_book_list + other_book_list
+
 			puts "\nEnter a book number to see what a loon George R.R. Martin is, or push 0 to return to main menu"
 			puts "\n\n\n\n\n"
-			input = STDIN.gets.chomp
-			if input == "0"
+			input = STDIN.gets.chomp.to_i
+			if input == 0
 				is_running = false
-			elsif 0 < input.to_i && input.to_i < 12
+			elsif 0 < input && input < 12
 				puts "\n\n\n\n\n\n\n\n\n"
 				DownArrow.display
-				puts "#{book_list[input.to_i - 1].name} has #{book_list[input.to_i - 1].characters.count} characters fighting for the throne."
+				selected_book = book_list[input - 1]
+				puts "#{selected_book.name} has #{selected_book.characters.count} characters fighting for the throne."
 				puts "\nWHAT A SCUFFLE!!!"
 				puts "\n\n"
 			else
@@ -181,12 +200,13 @@ class CLI
 	    input = STDIN.gets.chomp
 		  if input.starts_with? "y" || input == "yes"
 		  	puts "Give me your best guess.."
-		    input = STDIN.gets.chomp
-			  if input == "#{House.all.count}"
+		    input = STDIN.gets.chomp.to_i
+				answer = House.all.count
+			  if input == answer
 			  	puts "Congratulations, you are officially a G.O.T NERD"
 			    is_running = false
 			  else
-			    puts "Nice try, the correct Answer is #{House.all.count}"
+			    puts "Nice try, the correct Answer is #{answer}"
 					puts "Onward to the characters so you can NERD UP!!"
 					is_running = false
 				end
@@ -209,20 +229,16 @@ class CLI
 				Enter a house name to find out their Coat of Arms, or press 0 to return to main menu
 				====================================================================================
 				Suggested Searches (copy and paste works best!):
-				==============================================
-
-				House Targaryen of King's Landing
-
-				House Baratheon of King's Landing
-
-				House Stark of Winterfell
-
-				House Lannister of Casterly Rock
-
-				House Baelish of Harrenhal
-				=================================
+				================================================
 
 			HOUSE_NAME
+			House.all.sample(5).pluck(:name).each do |house_name|
+				puts house_name
+				puts
+			end
+
+			puts "================================="
+
 	    input = STDIN.gets.chomp
 	    puts "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
 	    puts "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
